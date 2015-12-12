22a91478efd754f762538bcf5c2994e641494fb0
taroooyan <taroooyan@gmail.com>
2015-12-12 14:43:22 +0900

    delete comment

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -1,20 +1,10 @@
-void setup()
-{
+void setup() {
 	Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
 	int value = analogRead(0);
 	int distance = (6787/(value-3))-4;
-	//50cm以下で反応
-	/* if(distance <= 50){ */
-	/* 	Serial.print(distance); */
-	/* 	Serial.println("cm"); */
-	/* } */
-	/* else{ */
-	/* 	Serial.println("not found"); */
-	/* } */
   Serial.println(distance);
 	delay(500);
 }
